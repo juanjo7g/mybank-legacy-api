749106968338b64f855533f227aeb656a9e5c968
Juan <tinez7g@gmail.com>
2016-04-29 16:23:26 -0500

    Se agrega datos del servidor firebase de produccion y se comentan los del servidor de prueba

--- config.js
+++ config.js
@@ -4,7 +4,13 @@ config.web = {};
 config.firebase = {};
 
 config.web.PORT = process.env.WEB_PORT || 8080;
-config.firebase.URL = "https://mybank-pruebas.firebaseio.com/";
-config.firebase.SECRET = "NuSJXKMDgJeCymeX5tjLu5GkjETrL257eBBQEGMq";
+
+// Firebase para pruebas:
+// config.firebase.URL = "https://mybank-pruebas.firebaseio.com/";
+// config.firebase.SECRET = "NuSJXKMDgJeCymeX5tjLu5GkjETrL257eBBQEGMq";
+
+// Firebase producción:
+config.firebase.URL = "https://mybankapp.firebaseio.com/";
+config.firebase.SECRET = "Iz0jgNOaHJyBeQaQdQgU95UctzutLhzKXg8sUf0g";
 
 module.exports = config;
