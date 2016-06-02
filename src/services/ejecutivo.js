61914b3e984d1a30100aca4ce99eabb440eaaa5b
Juan <tinez7g@gmail.com>
2016-06-02 16:34:44 -0500

    Se añade metodo a la estructura de services/ejecutivo.js

--- src/services/ejecutivo.js
+++ src/services/ejecutivo.js
@@ -12,4 +12,9 @@ router.get('/get/:tipo_doc/:documento', function (req, res){
   return res.status(200);
 });
 
+// Metodo para consultar la información del ejecutivo como cliente
+router.get('/get/:email', function (req, res){
+  return res.status(200);
+});
+
 module.exports = router;
