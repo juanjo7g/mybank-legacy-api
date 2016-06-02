ed8618a855235444ab4f24e88d56c08572564da5
Juan <tinez7g@gmail.com>
2016-06-02 18:39:43 -0500

    Se agrega metodo para obtener lista de movimientos de un producto y se valida que el producto sea del cliente que va a consultar. Se inicia el envio de mensaje por email y sms al consultar detalles de un producto (Obtener lista de movimientos del producto)

---
+++ src/util.js
@@ -0,0 +1,7 @@
+exports.enviarEmail = function(correo_cliente) {
+    console.log("Enviando correo a : " + correo_cliente);
+}
+
+exports.enviarSMS = function(correo_cliente) {
+    console.log("Enviando SMS a : " + correo_cliente);
+}
--- test/services/movimiento_test.js
+++ test/services/movimiento_test.js
@@ -9,3 +9,28 @@ var token = tokenGenerator.createToken({ uid: "uniqueId1", some: "arbitrary", da
 
 var PUERTO = process.env.PORT || 8080;
 var server = supertest.agent("http://localhost:" + PUERTO);
+
+describe("Prueba Unitaria services/movimiento.js",function(){
+  this.timeout(7000);
+
+  it("Prueba metodo get lista de movimientos por producto_id con token valido",function(done){
+    server
+    .get("/api/v2/movimiento/get/123/email@test.com?token=" + token)
+    .expect(200)
+    .end(function(err,res){
+      res.status.should.equal(200);
+      done();
+    });
+  });
+
+  it("Prueba metodo get lista de movimientos por producto_id con token invalido",function(done){
+    server
+    .get("/api/v2/movimiento/get/123/email@test.com?token=123")
+    .expect(401)
+    .end(function(err,res){
+      res.status.should.equal(401);
+      done();
+    });
+  });
+
+});
