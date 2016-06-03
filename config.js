294fe7d93d4479e848c8b459977b806a464d5500
Juan <tinez7g@gmail.com>
2016-06-02 22:40:09 -0500

    Se termina la funcionalidad de enviar email y SMS cuando se consultan los detalles de un producto, se usa nodemailer para enviar los email y se usa twilio para los SMS.

--- config.js
+++ config.js
@@ -4,6 +4,8 @@ var config = {};
 config.web = {};
 config.firebase = {};
 config.postgres = {};
+config.admin = {};
+config.twilio = {};
 
 config.web.PORT = process.env.WEB_PORT || 8080;
 
@@ -21,5 +23,14 @@ config.firebase.SECRET = "lYUPmUzN7lQJyUWwgYMtd3kJdcwcscF8s6P81Gv6";
 
 // Postgres producción:
 config.postgres.URL = "postgres://jvmxzymhrluxqy:zceP-nVFulfhGbTPIRhe3hD2EZ@ec2-23-21-112-245.compute-1.amazonaws.com:5432/ddb2ed3sj3c8v3";
+
+// Credenciales email admin:
+config.admin.EMAIL = "mybank.legacyapi@gmail.com";
+config.admin.PASSWORD = "IIpnnsa7LnbQwUO";
+
+// Twilio producción:
+config.twilio.ACCOUNT_SID = "AC7a68bb37b24dea90ad614605d12e1f9d";
+config.twilio.AUTH_TOKEN = "5557432bf68cda9242c72fafb58accc3";
+config.twilio.PHONE_NUMBER = "+12054099328"
 
 module.exports = config;
