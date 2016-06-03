0f19f117e9d7b52a752fa741c2905ac91f9824ec
Bonampa312 <ralp2089@gmail.com>
2016-06-03 12:05:39 -0500

    metodos obtener movimiento por id, metodos de ejecutivo

--- src/services/ejecutivo.js
+++ src/services/ejecutivo.js
@@ -9,11 +9,78 @@ pg.defaults.ssl = true;
 
 // Metodo para consultar la información personal como ejecutivo
 router.get('/get/:tipo_doc/:documento', function (req, res){
-  return res.status(200);
+  // Realizando la consulta a la base de datos.
+  var tipo_doc = req.params.tipo_doc;
+  var documento = routes.documento;
+  if(documento == undefined){
+    documento = req.params.documento;
+  }
+  var queryGet = 'SELECT * FROM ejecutivo WHERE tipo_doc = \'' + tipo_doc + '\' and documento = \'' + documento + '\';';
+  var URL = config.postgres.URL;
+  var results = [];
+
+  // Conectando base de datos y ejecutando el query.
+  pg.connect(URL, function(err, client, done){
+    if(err) {
+      done();
+      console.log(err);
+      return res.status(500).json({ success: false, data: "ERROR en la base de datos"});
+    }
+    var query = client.query(queryGet, function(err, result){
+      if (err){
+        done();
+        console.log(err);
+        return res.status(400).json({ success: false, data: "ERROR en el query"});
+      }
+      console.log('Consuta exitosa.');
+      client.end();
+    });
+    query.on('row', function(row){
+      results.push(row);
+    });
+    query.on('end', function() {
+      done();
+      return res.status(200).json(results);
+    });
+  });
+    return res.status(200);
 });
 
 // Metodo para consultar la información del ejecutivo como cliente
 router.get('/get/:email', function (req, res){
+  // Realizando la consulta a la base de datos.
+  var email = routes.email;
+  if(email == undefined){
+    email = req.params.email;
+  }
+  var queryGet = 'SELECT * FROM ejecutivo WHERE correo = \'' + email + '\';';
+  var URL = config.postgres.URL;
+  var results = [];
+
+  // Conectando base de datos y ejecutando el query.
+  pg.connect(URL, function(err, client, done){
+    if(err) {
+      done();
+      console.log(err);
+      return res.status(500).json({ success: false, data: "ERROR en la base de datos"});
+    }
+    var query = client.query(queryGet, function(err, result){
+      if (err){
+        done();
+        console.log(err);
+        return res.status(400).json({ success: false, data: "ERROR en el query"});
+      }
+      console.log('Consuta exitosa.');
+      client.end();
+    });
+    query.on('row', function(row){
+      results.push(row);
+    });
+    query.on('end', function() {
+      done();
+      return res.status(200).json(results);
+    });
+  });
   return res.status(200);
 });
 
--- test/services/ejecutivo_test.js
+++ test/services/ejecutivo_test.js
@@ -9,3 +9,48 @@ var token = tokenGenerator.createToken({ uid: "uniqueId1", some: "arbitrary", da
 
 var PUERTO = process.env.PORT || 8080;
 var server = supertest.agent("http://localhost:" + PUERTO);
+
+
+describe("Prueba Unitaria services/ejecutivo.js",function(){
+  this.timeout(7000);
+
+  it("Prueba metodo get ejecutivo como cliente por el email con token valido",function(done){
+    server
+    .get("/api/v2/ejecutivo/get/email@test.com?token=" + token)
+    .expect(200)
+    .end(function(err,res){
+      res.status.should.equal(200);
+      done();
+    });
+  });
+
+  it("Prueba metodo get ejecutivo como cliente por el email con token invalido",function(done){
+    server
+    .get("/api/v2/ejecutivo/get/email@test.com?token=123")
+    .expect(401)
+    .end(function(err,res){
+      res.status.should.equal(401);
+      done();
+    });
+  });
+
+  it("Prueba metodo get por tipo y numero de documento con token valido",function(done){
+    server
+    .get("/api/v2/ejecutivo/get/cc/documento?token=" + token)
+    .expect(200)
+    .end(function(err,res){
+      res.status.should.equal(200);
+      done();
+    });
+  });
+
+  it("Prueba metodo get por tipo y numero de documento con token invalido",function(done){
+    server
+    .get("/api/v2/ejecutivo/get/cc/documento?token=123")
+    .expect(401)
+    .end(function(err,res){
+      res.status.should.equal(401);
+      done();
+    });
+  });
+});
