bc59254612da1c25bf28c68091ddd4e1e4fe758c
tinez7g <tinez7g@users.noreply.github.com>
2016-06-09 22:18:34 -0500

    Se hace conexión al Firebase de producción

--- config.js
+++ config.js
@@ -13,13 +13,13 @@ config.web.PORT = process.env.WEB_PORT || 8080;
 // config.firebase.URL = "https://mybank-pruebas.firebaseio.com/";
 // config.firebase.SECRET = "NuSJXKMDgJeCymeX5tjLu5GkjETrL257eBBQEGMq";
 
-// Firebase para pruebas:
-config.firebase.URL = "https://mybankatom.firebaseio.com/";
-config.firebase.SECRET = "lYUPmUzN7lQJyUWwgYMtd3kJdcwcscF8s6P81Gv6";
+// Firebase para pruebas IONIC:
+// config.firebase.URL = "https://mybankatom.firebaseio.com/";
+// config.firebase.SECRET = "lYUPmUzN7lQJyUWwgYMtd3kJdcwcscF8s6P81Gv6";
 
 // Firebase producción:
-// config.firebase.URL = "https://mybankapp.firebaseio.com/";
-// config.firebase.SECRET = "Iz0jgNOaHJyBeQaQdQgU95UctzutLhzKXg8sUf0g";
+config.firebase.URL = "https://mybankapp.firebaseio.com/";
+config.firebase.SECRET = "Iz0jgNOaHJyBeQaQdQgU95UctzutLhzKXg8sUf0g";
 
 // Postgres producción:
 config.postgres.URL = "postgres://jvmxzymhrluxqy:zceP-nVFulfhGbTPIRhe3hD2EZ@ec2-23-21-112-245.compute-1.amazonaws.com:5432/ddb2ed3sj3c8v3";
